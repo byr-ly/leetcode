35e82d90d83b1e6a2e6ef7834f34a6a681e58712
liyang <liyang@liyangdeMacBook-Pro.local>
2017-02-25 17:44:26 +0800

    add

--- leetcode_ly/java/Tree/BinaryTreeInorderTraversal.java
+++ leetcode_ly/java/Tree/BinaryTreeInorderTraversal.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ //
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<Integer>();
@@ -21,5 +22,82 @@ public class Solution {
         list.add(root.val);
         inorder(root.right,list);
         return;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ //O(N)
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        if(root == null) return res;
+        inorder(root,s,res);
+        return res;
+    }
+    
+    public void inorder(TreeNode root,Stack<TreeNode> s,List<Integer> res){
+        while(root != null || !s.isEmpty()){
+            s.push(root);
+            root = root.left;
+            while(root == null && !s.isEmpty()){
+                root = s.pop();
+                res.add(root.val);
+                root = root.right;
+            }
+        }
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ //O(1)
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root == null) return res;
+        inorder(root,res);
+        return res;
+    }
+    
+    public void inorder(TreeNode root,List<Integer> res){
+        TreeNode cur = root;
+        TreeNode pre = null;
+        while(cur != null){
+            if(cur.left == null){
+                res.add(cur.val);
+                cur = cur.right;
+            }
+            else{
+                pre = cur.left;
+                while(pre.right != null && pre.right != cur){
+                    pre = pre.right;
+                }
+                if(pre.right == null){
+                    pre.right = cur;
+                    cur = cur.left;
+                }
+                else{
+                    pre.right = null;
+                    res.add(cur.val);
+                    cur = cur.right;
+                }
+            }
+        }
     }
 }
--- leetcode_ly/java/Tree/BinaryTreePostorderTraversal.java
+++ leetcode_ly/java/Tree/BinaryTreePostorderTraversal.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ //
 public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list =  new ArrayList<Integer>();
@@ -21,5 +22,42 @@ public class Solution {
         postorder(root.right,list);
         list.add(root.val);
         return;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ //
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        if(root == null) return res;
+        postorder(root,s,res);
+        return res;
+    }
+    
+    public void postorder(TreeNode root,Stack<TreeNode> s,List<Integer> res){
+        s.push(root);
+        TreeNode pre = null;
+        while(!s.isEmpty()){
+            root = s.peek();
+            if((root.left == null && root.right == null) || (pre != null && (root.left == pre || root.right == pre))){
+                res.add(root.val);
+                s.pop();
+                pre = root;
+            }
+            else{
+                if(root.right != null) s.push(root.right);
+                if(root.left != null) s.push(root.left);
+            }
+        }
     }
 }
